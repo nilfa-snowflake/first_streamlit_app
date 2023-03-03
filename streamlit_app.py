fbaeb5077c540db0db4a442c85e31799a580dfa2
nilfa-snowflake <125874063+nilfa-snowflake@users.noreply.github.com>
2023-03-03 22:27:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,5 +31,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit list contains:")
-streamlit.text(my_data_row)
+
 streamlit.dataframe(my_data_row)
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.text("thanks for adding ",fruit_choice)
